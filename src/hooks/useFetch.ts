fe7c0a4fc44d8d14bedf29669ae5aaf6c06ccd90
Gabriel Loureiro <gabrieloureirof@gmail.com>
2021-02-13 03:16:24 -0300

    add useFetch custom hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import useSWR from 'swr'
+
+import api from '@/services/api'
+
+export function useFetch<Data = any, Error = any>(url: string) {
+  const { data, error, mutate } = useSWR<Data, Error>(url, async url => {
+    const response = await api.get(url)
+
+    return response.data
+  })
+
+  return { data, error, mutate }
+}
