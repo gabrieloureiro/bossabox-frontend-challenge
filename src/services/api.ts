0cc4556d04fade5e16eb2478b887052952d7eeb0
Gabriel Loureiro <gabrieloureirof@gmail.com>
2021-02-17 23:31:18 -0300

    fix scrips and baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://tools-bossabox-challenge.herokuapp.com/'
-      : 'http://localhost:3333/',
+  baseURL: 'https://tools-bossabox-challenge.herokuapp.com/',
   responseType: 'json',
   headers: { 'content-type': 'application/json' }
 })
