0e50034ece2bb0e255eef9d0d55b42f32a4e1ea8
Gabriel Loureiro <gabrieloureirof@gmail.com>
2021-02-17 23:50:04 -0300

    trying to enable cors on axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,10 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BOSSABOX_API,
   responseType: 'json',
-  headers: { 'content-type': 'application/json' }
+  headers: {
+    'content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 
 export default api
